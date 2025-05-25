9e9398da821c1de596edacfe6cebd1b3aed96227
Roman Podymov <podymfrombryansk@yandex.com>
2025-05-25 16:40:23 +0200

    Nullable

--- lib/main.dart
+++ lib/main.dart
@@ -73,16 +73,16 @@ class _MyHomePageState extends State<MyHomePage> {
 }
 
 class Cat {
-  final String id;
-  final String url;
-  final int width;
-  final int height;
+  final String? id;
+  final String? url;
+  final int? width;
+  final int? height;
 
   const Cat({required this.id, required this.url, required this.width, required this.height});
 
   factory Cat.fromJson(Map<String, dynamic> json) {
     return switch (json) {
-      {'id': String id, 'url': String url, 'width': int width, 'height': int height} => Cat(
+      {'id': String? id, 'url': String? url, 'width': int? width, 'height': int? height} => Cat(
         id: id,
         url: url,
         width: width,
--- test/test.dart
+++ test/test.dart
@@ -4,11 +4,21 @@ import 'package:test/test.dart';
 void main() {
   group('Tests', () {
     test('Parse cat', () {
+      final expectedId = 'some id';
+      final expectedUrl = 'some url';
+      final expectedWidth = 200;
+      final expectedHeight = 100;
       final raw = <String, dynamic>{
-        'id': 'some id'
+        'id': expectedId,
+        'url': expectedUrl,
+        'width': expectedWidth,
+        'height': expectedHeight
       };
       final cat = Cat.fromJson(raw);
-      expect(cat.id, 'some id');
+      expect(cat.id, expectedId);
+      expect(cat.url, expectedUrl);
+      expect(cat.width, expectedWidth);
+      expect(cat.height, expectedHeight);
     });
   });
 }
